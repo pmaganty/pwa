1c917f4eb5fc12c40ad67853d313622cc532f433
pmaganty <pranithamaganty@gmail.com>
2020-04-22 17:39:43 -0500

    offline functionality

---
+++ public/service-worker.js
@@ -0,0 +1,69 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/db.js",
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+  ];
+  
+  
+  const PRECACHE = "precache-v1";
+  const RUNTIME = "runtime";
+  
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open(PRECACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+        return Promise.all(cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        }));
+      }).then(() => self.clients.claim())
+    );
+  });
+  
+  // fetch
+self.addEventListener("fetch", function(evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(RUNTIME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+      })
+    );
+  });
---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({body}, res) => {
+  console.log("INSIDE API/TRANSACTION/BULK");
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      console.log(dbTransaction);
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      console.log("ERRORED OUT");
+      res.status(404).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
